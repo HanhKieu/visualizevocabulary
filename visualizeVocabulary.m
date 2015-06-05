30ce92f07ee470f8d36323fb406323a2a22b8dfe
Hanh Kieu <hckieu@ucdavis.edu>
2015-06-04 21:33:10 -0700

    doing other half two

--- visualizeVocabulary.m
+++ visualizeVocabulary.m
@@ -1,40 +1,56 @@
-addpath('./provided_code/');
+addpath(genpath('../'));
 
 framesDirectory = '../frames';
-siftDirectory = '../sift/';
+siftDirectory = '../sift';
 
-% Get a list of all the .mat files in that directory.
-% There is one .mat file per image.
-fileStructs = dir([siftdir '/*.mat']); %struct
-fprintf('reading %d total files...\n', length(fnames));
-
+fileStructs = dir([siftDirectory '/*.mat']); %struct
+% fprintf('reading %d total files...\n', length(fileStructs));
+% 
+% myDescriptors = [];
+% myPositions = [];
+% myScales = [];
+% myOrients = [];
+% for i=1:length(fileStructs)
+% 
+%     fprintf('reading frame %d of %d\n', i, length(fileStructs));
+% 
+%     load(fileStructs(i).name)
+%     numDescriptors = size(descriptors,1); %NUMBER OF ROWS OUR DESCRIPTOR HAS, BASICALLY THE NUMBER OF DESCRIPTORS IN OUR DESCRIPTOR MATRI
+% 
+%    if(numDescriptors > 0 )
+%     numDescriptorsToCollect = min(20, numDescriptors);
+%     descriptorIndexes = randperm(numDescriptors,numDescriptorsToCollect);
+%     
+%     myDescriptors = [myDescriptors; descriptors(descriptorIndexes,:)];
+%     myPositions = [myPositions ;positions(descriptorIndexes,:)];
+%     myScales = [myScales ; scales(descriptorIndexes,:)];
+%     myOrients = [myOrients;orients(descriptorIndexes,:)];
+%    end
+%    
+%    
+%    
+% 
+% end 
+%     %load('myVariables.mat')
+%     k = 1500; %BEST VALUE TO USE FOR OUR PURPOSES
+%    [idx, C] = kmeansML(k,myDescriptors');
+%    C = C';
+load('myVariables.mat')
+counter = 0;
 for i=1:length(fileStructs)
-
-    fprintf('reading frame %d of %d\n', i, length(fnames));
-    % load that file
-    fileSift = [siftDirectory '/' fileStructs(i).name];  %GETS THE MATLAB FILE
-    fileSift(fname, 'imname', 'descriptors', 'positions', 'scales', 'orients'); %THE LOAD FUNCTION LOADS THESE VARIABLES FROM fileSift
-    numDescriptors = size(descriptors,1); %NUMBER OF ROWS OUR DESCRIPTOR HAS, BASICALLY THE NUMBER OF DESCRIPTORS IN OUR DESCRIPTOR MATRI
-    if(numDescriptors > 0) %ONLY GATHER DESCRIPTORS IF THERE ARE DESCRIPTORS
-        if(numDescriptors > 20) 
-           numOfDescriptorsToCollect = 20; %IF WE HAVE MORE THAN 20 DESCRIPTORS, WE ONLY WANT 20 DESCRIPTORS
-        end
+    load(fileStructs(i).name)
+    numDescriptors = size(descriptors,1);
+    for j=1:numDescriptors %goes through each descriptor in selected Image
+        closestClusterMatrix = dist2(descriptors(j,:), C); %find the closest cluster or "word" it belongs to
+        [M,I] = min(closestClusterMatrix);
         
         
-        
-        
-      
-       for i=1:numOfDescriptorsToCollect
-           myDescriptors = [myDescriptors; descriptors(1:numOfDescriptorsToCollect,:)];
-           myPositions = [myPositions, positions(1:numOfDescriptorsToCollect,:)];
-           myScales = [myScales, scales(1:numOfDescriptorsToCollect,:)];
-           myOrients = [myOrients, orients(1:numOfDescriptorsToCollect,:)];
-       end
-    
-    end  
-
+    end
 end
-    k = 1500; %BEST VALUE TO USE FOR OUR PURPOSES
-   [idx, C] = kmeansML(k,single(allDescriptors));
-   
+
+        
+    
+    
+    
+
    
